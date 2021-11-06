041f728d6ffab38261f9bfcc8d3709f757901b64
Joker-5 <2725284321@qq.com>
2021-11-06 23:13:33 +0800

    add c&s instance

--- netty-quick-demo/src/main/java/com/joker/netty/client/QuickClient.java
+++ netty-quick-demo/src/main/java/com/joker/netty/client/QuickClient.java
@@ -1,4 +1,28 @@
 package com.joker.netty.client;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.util.Date;
+
 public class QuickClient {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder());
+                    }
+                })
+                .connect("127.0.0.1", 8080)
+                .sync()
+                .channel()
+                .writeAndFlush(new Date() + ": hello world");
+    }
 }
--- netty-quick-demo/src/main/java/com/joker/netty/server/QuickServer.java
+++ netty-quick-demo/src/main/java/com/joker/netty/server/QuickServer.java
@@ -1,4 +1,38 @@
 package com.joker.netty.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
 public class QuickServer {
+    public static void main(String[] args) {
+        // 启动器，负责组装netty组件，启动服务器
+        new ServerBootstrap()
+                //
+                .group(new NioEventLoopGroup())
+                // 选择服务器的ServerSocketChannel实现
+                .channel(NioServerSocketChannel.class)
+                // 代表和客户端进行数据读写的通道
+                .childHandler(
+                        new ChannelInitializer<NioSocketChannel>() {
+                            @Override
+                            protected void initChannel(NioSocketChannel ch) {
+                                // 添加具体的handler
+                                ch.pipeline().addLast(new StringDecoder());
+                                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                    @Override
+                                    // 读事件
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        System.out.println(msg);
+                                    }
+                                });
+                            }
+                        })
+                .bind(8080);
+    }
 }
