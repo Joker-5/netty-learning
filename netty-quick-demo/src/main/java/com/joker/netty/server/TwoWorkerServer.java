8560b95a134eb112b120c94188151e0cee7e1439
Joker-5 <2725284321@qq.com>
2021-11-08 22:26:28 +0800

    add: two eventLoopGroup demo

--- netty-quick-demo/src/main/java/com/joker/netty/server/TwoWorkerServer.java
+++ netty-quick-demo/src/main/java/com/joker/netty/server/TwoWorkerServer.java
@@ -1,4 +1,40 @@
 package com.joker.netty.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.ByteBuffer;
+
+@Slf4j
 public class TwoWorkerServer {
+    public static void main(String[] args) throws InterruptedException {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup(1), new NioEventLoopGroup(2))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf byteBuf = msg instanceof ByteBuf ? ((ByteBuf) msg) : null;
+                                if (byteBuf != null) {
+                                    byte[] buf = new byte[16];
+                                    ByteBuf len = byteBuf.readBytes(buf, 0, byteBuf.readableBytes());
+                                    log.debug(new String(buf));
+                                }
+                            }
+                        });
+                    }
+                })
+                .bind(8080)
+                .sync();
+    }
 }
